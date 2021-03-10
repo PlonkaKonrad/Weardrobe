ca70141321a1d96a2e0c6bdd67635eab39b462eb
PlonkaKonrad <konrad66a4@gmail.com>
2021-03-10 15:55:30 +0100

    add fetch

--- weardrobe/src/components/App.js
+++ weardrobe/src/components/App.js
@@ -4,18 +4,50 @@ import Display from './display/display';
 import Wordrobe from './wordrobe/wordrobe'
 
 
+const gender = ['man','woman']
+const category = ['all', '']
+
 
 class App extends Component {
   state = {
-    gender: '',
-    style: '', 
+    sex: '',
+    category: '',
+    productsList: []
     }
 
-    componentDidMount() {
-      fetch("https://www2.hm.com/pl_pl/on/nowosci/view-all/_jcr_content/main/productlisting.display.json?sort=stock&image-size=large&image=alt&offset=36&page-size=200")
-    .then(response => response.json())
-    .then(data => console.log(data));
+   componentDidMount() {
+      fetch("https://apidojo-hm-hennes-mauritz-v1.p.rapidapi.com/products/list?country=pl&lang=pl&currentpage=0&pagesize=200&categories=men_trousers&concepts=H%26M%20MAN", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "a91d8e162amsh76cb93ee6c1ea04p10c773jsn656c6b8f2df7",
+		"x-rapidapi-host": "apidojo-hm-hennes-mauritz-v1.p.rapidapi.com"
+	}
+    })
 
+//   fetch("https://apidojo-hm-hennes-mauritz-v1.p.rapidapi.com/products/detail?lang=pl&productcode=0967497005&country=pl", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-key": "a91d8e162amsh76cb93ee6c1ea04p10c773jsn656c6b8f2df7",
+// 		"x-rapidapi-host": "apidojo-hm-hennes-mauritz-v1.p.rapidapi.com"
+// 	}
+// })
+
+
+
+  .then(response => response.json())
+  .then(data => {
+    
+    const productsList= data.results;
+
+    console.log(data.results)
+
+      this.setState({
+        productsList: productsList,
+
+      })
+
+  });
+     
 
     }
 
@@ -24,7 +56,7 @@ class App extends Component {
       <>
       <Menu />
       <Display />
-      <Wordrobe />
+      <Wordrobe productsList={this.state.productsList} />
 
 
 
--- weardrobe/src/components/wordrobe/wordrobe.js
+++ weardrobe/src/components/wordrobe/wordrobe.js
@@ -2,10 +2,32 @@ import React from 'react';
 import './wordrobe.css';
 
 
-const Wordrobe = () => {
+
+
+
+
+
+
+
+
+
+const Wordrobe = (props) => {
+
+    const productsList = props.productsList;
+       
+
+    const productsListMap = productsList.map(product => (
+
+        <div key={product.pk} className='product'><img src={product.images[0].url} alt={product.name}/></div>
+
+    ))
+
+
     return ( 
         <div className='wordrobe'>
-            Wyświetl wszystkie ubrania wybieraj i po kliknięciu lub przeciągnięciu je dodawaj do krauzeli w komponencie display
+           
+            {productsListMap}
+           
 
         </div>
      );
